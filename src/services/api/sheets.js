1ecc6786b4bccd30619642b28869d7014da05f6b
Dilip <162571689+dilip2k04@users.noreply.github.com>
2026-05-24 22:22:52 +0530

    changed detials

--- src/services/api/sheets.js
+++ src/services/api/sheets.js
@@ -1,5 +1,5 @@
 export const SPREADSHEET_ID =
-  "1ShLNlhBe_dh4Cot8ZG4VfNSG9xgMFPACcmslWJx0Irc";
+  "1Nc_XqnrtVChMVIvmuIIIImqTDViOQ10Fy9oVrXpX_2o";
 
 export const BASE_URL =
   `https://opensheet.elk.sh/${SPREADSHEET_ID}`;
